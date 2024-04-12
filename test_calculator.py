13b088b1333e34b78ed69524d384159ee72a1bca
VotreNomUtilisateur <votre@email.com>
2024-04-12 16:29:12 +0200

    test calculator

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+from calculator import addition, soustraction, division
+
+
+
+
+def test_addition():
+    assert addition(1, 2) == 3
+
+def test_soustraction():
+    assert soustraction(2, 1) == 1
+    
+def test_division():
+    assert division(2, 1) == 2
